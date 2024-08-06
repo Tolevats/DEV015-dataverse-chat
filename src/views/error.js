fa5cebe84868b279026020fabb38047387de44fb
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-08-06 11:48:56 -0400

    add header & footer to error.js

--- src/views/error.js
+++ src/views/error.js
@@ -1,23 +1,32 @@
-//import { Header } from "../components/header.js";
+import { Header } from "../components/header.js";
 import { navigateTo } from "../router.js";
-//import { Footer } from "../components/footer.js";
+import { Footer } from "../components/footer.js";
 
-//<i class="fa-solid fa-robot"></i>
-export default () => {
+export const Fail = () => {
   const viewEl = document.createElement('div');
-  viewEl.innerHTML = `
-    /* <link rel="stylesheet" href="styleError.css" /> */
-        <i class="fa-solid fa-meteor" style="color: #e32400;"></i>
-        <h1 class="errorNum">404</h1>
-        <h2 class="headingError">¡Ups! Página no encontrada</h2>
-        <picture class="sciFiError">
-            <img src="assets/img/spacerocket2.png" alt="space rocket crashing">
-        </picture>
-        <button id="backHome">Volver a home</button>
-      `
+  // Crear y añadir Header
+  const headerEl = Header();
+  viewEl.appendChild(headerEl);
+
+  // Crear y añadir contenido principal de la página de error
+  const mainContent = document.createElement('div');
+  mainContent.innerHTML = `
+  <h1 class="errorNum">404</h1>
+  <h2 class="headingError">¡Ups! Página no encontrada</h2>
+  <picture class="sciFiError">
+      <img src="assets/img/spacerocket2.png" alt="space rocket crashing">
+  </picture>
+  <button id="backHome">Volver a home</button>
+`;
+  viewEl.appendChild(mainContent);
+
+  // Crear y añadir Footer
+  const footerEl = Footer();
+  viewEl.appendChild(footerEl);
+
+  //Botón para regresar a Home
   const buttonBackHome = viewEl.querySelector('#backHome');
   buttonBackHome.addEventListener('click', () => navigateTo("/"));
   
-  
   return viewEl;
-}
+};
