431f31c7d1b9100c6f1c24907f62d33e22807604
carolina <carolinartz5991@gmail.com>
2024-08-07 10:20:03 -0500

    Feacture:configuration OPENAI

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,9 +1,49 @@
-// src/lib/openAIApi.js
-
-// Importa la función para obtener la API KEY desde apiKey.js
 import { getApiKey } from './apiKey.js';
 
+const OPENAI_API_KEY = getApiKey();
 
 export const communicateWithOpenAI = (messages) => {
-   //Aquí es donde debes implementar la petición con fetch o axios
+  return new Promise((resolve, reject) => { // retorna una nueva promesa
+    if (!OPENAI_API_KEY) { // si OPENAI_API_KEY no está definida
+      const error = new Error("No se ha ingresado contraseña de API");
+      return reject(error);
+    }
+
+    const url = "https://api.openai.com/v1/chat/completions"; // URL correcta de la API de OpenAI
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${OPENAI_API_KEY}`,
+    };
+    const body = JSON.stringify({
+      messages: messages,
+      model: "gpt-3.5-turbo",
+    });
+
+    // HACER SOLICITUD
+    fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: body,
+    })
+    // MANEJAR LAS RESPUESTAS
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 401) {
+            throw new Error("Verificar clave API");
+          } else {
+            throw new Error("No se puede realizar la solicitud");
+          }
+        }
+        return response.json();
+      })
+    // RESUELVE LA PROMESA
+      .then((data) => {
+        resolve(data.choices[0].message.content);
+      })
+    // MANEJO DE ERRORES
+      .catch((error) => {
+        console.error("Error al hacer la solicitud:", error);
+        reject(error);
+      });
+  });
 };
